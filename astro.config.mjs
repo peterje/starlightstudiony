01b73305c6ca7f8ea4d702d70cb6d9fbf15055f1
Peter Edmonds <petejedmonds@gmail.com>
2023-01-21 14:12:58 -0800

    +images +pricing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,8 +12,11 @@ import image from "@astrojs/image";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), image()],
+  integrations: [tailwind(), image(), sitemap()],
   adapter: vercel()
 });
