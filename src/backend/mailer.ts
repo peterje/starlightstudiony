2a3173bc03475eb22bfaa9e435d7c4c1bf98af32
Peter Edmonds <petejedmonds@gmail.com>
2023-01-29 13:23:55 -0800

    added messaging system

--- src/backend/mailer.ts
+++ src/backend/mailer.ts
@@ -1,5 +1,52 @@
-import type {BookingRequest} from "../pages/api/book.ts"
 import Mailjet, { SendEmailV3_1, LibraryResponse } from "node-mailjet"
+import type { MessageRequest } from "../pages/api/message"
+
+export const send_message_email = async (message_request: MessageRequest) => {
+	const public_key = import.meta.env.MAILJET_PUBLIC_KEY as string
+	const private_key = import.meta.env.MAILJET_PRIVATE_KEY as string
+	const mailjet = new Mailjet({apiKey: public_key, apiSecret: private_key})
+	
+	const email_data: SendEmailV3_1.Body = {
+	    Messages: [
+	      {
+		From: {
+		  Email: 'starlightstudio@starlightstudiony.com',
+		},
+		To: [
+		  {
+		    Email: 'petejedmonds@gmail.com',
+		  },
+		],
+		TemplateLanguage: true,
+		TemplateErrorReporting: {
+		  Email: 'petejedmonds@gmail.com',
+		  Name: 'Reporter',
+		},
+		Subject: 'You have a new customer message.',
+		HTMLPart: `
+		<body>
+			<h1>You have a new message:</h1>
+			<ul>
+
+			<li>Customer Name: ${message_request.name}</li>
+			<li>Customer Email: ${message_request.email}</li>
+			<li>Customer Phone: ${message_request.phone}</li>
+
+			</ul>
+			<p>
+			${message_request.message}
+						</p>
+
+		</body>
+		`,
+		TextPart: 'Dear passenger, welcome to Mailjet! May the delivery force be with you!',
+	      },
+	    ],
+	}
+	await mailjet
+          .post('send', { version: 'v3.1' })
+	  .request(email_data)
+}
 
 export const send_booking_email = async (booking_request: BookingRequest) => {
 	const public_key = import.meta.env.MAILJET_PUBLIC_KEY as string
@@ -10,7 +57,7 @@ export const send_booking_email = async (booking_request: BookingRequest) => {
 	    Messages: [
 	      {
 		From: {
-		  Email: 'peteredm@proton.me',
+		  Email: 'starlightstudio@starlightstudiony.com',
 		},
 		To: [
 		  {
---
+++ src/pages/api/message.ts
@@ -0,0 +1,26 @@
+import type { APIContext } from 'astro';
+import { send_message_email } from "../../backend/mailer"
+
+export type MessageRequest = {
+	name: string,
+	email: string,
+	phone: string,
+	message: string
+}
+
+export const post = async ({ request, redirect }: APIContext) => {
+	const data = await request.formData();
+	const message = {
+		name: data.get('full-name') as string,
+		email: data.get('email') as string,
+		phone: data.get('phone') as string,
+		message: data.get('message') as string,
+	} satisfies MessageRequest
+	try {
+		await send_message_email(message)
+		return redirect('/book?success=true');
+	} catch (error) {
+		return new Response("Failed to create booking", { status: 500 })
+	}
+}
+
